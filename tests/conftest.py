bdc99675e01bb697b3ee5224f1ae257ee40a2b63
unknown <rammalrashad210@gmail.com>
2026-01-05 02:58:01 +0200

    fix

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,10 +1,9 @@
-# tests/conftest.py
 import pytest
 from backend import app as backend_app, db
 
 @pytest.fixture(scope="session", autouse=True)
 def setup_database():
-    # Import inside the fixture so Postgres service is ready
+    # Import inside the fixture so Postgres is ready and DATABASE_URL is set
     application = backend_app.app  # or however you expose the Flask app
     with application.app_context():
         db.create_all()
