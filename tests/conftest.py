a6793bba685cd533f4ffef75f0d34fdc560acae8
unknown <rammalrashad210@gmail.com>
2026-01-06 16:03:22 +0200

    f3

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,5 +1,11 @@
 import os
 import pytest
+import sys
+import types
+
+# Provide a fake yolo_webcam module so imports succeed
+sys.modules['yolo_webcam'] = types.ModuleType('yolo_webcam')
+sys.modules['yolo_webcam'].run_ai_on_frame = lambda frame: None
 
 # 🔥 Force SQLite BEFORE app import
 os.environ["DATABASE_URL"] = "sqlite:///:memory:"
